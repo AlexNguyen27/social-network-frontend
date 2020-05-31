07b9f8ac625149aba33d8d890bc989c1efe54b89
Thanh Nguyen <thanhnguyen.tnn55@gmail.com>
2020-05-31 12:47:30 +0700

    Updated folder

---
+++ src/constants/Colors.js
@@ -0,0 +1,36 @@
+export default {
+    primary: '#f96d80',
+    accent: '#f8b24f',
+    star: '#ffd31d',
+    orange: '#f17808',
+    red: 'red',
+    view: '#79d70f',
+    statusBar: '#bb596b',
+    light_blue: '#f8b24f',
+  
+    linear1: '#fce8d5',
+    linear2: '#ffffff',
+    linear3: '#f0f0f0',
+    linear4: '#ffd3e1',
+  
+    txt_main: '#0765d7',
+    txt_description: '#777777',
+    txt_dark: '#3c3c3c',
+    txt1: 'white',
+  
+    bg_main: '#0765d7',
+    bg_header: '#f8f7f3',
+    bg1: 'white',
+  
+    bd_main: '#0765d7',
+    bd1: 'white',
+    bd_line: '#dddddd',
+    bd_input: '#cbcbcb',
+  
+    white: '#FDFDFD',
+    tuna: '#4B4E55',
+    malibu: '#4FB2E2',
+    light_gray: '#D1CECE',
+    dark_gray: '#888888',
+    ebb: '#E3DAD8',
+  };
