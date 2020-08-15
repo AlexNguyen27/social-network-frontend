949f36ef0970ac4f392765ca65e0cd0a0059fbb8
Thanh Nguyen <thanhnguyen.tnn55@gmail.com>
2020-08-15 16:18:06 +0700

    feat: add new feed ui

--- src/components/pages/newsFeed/NewsFeed.js
+++ src/components/pages/newsFeed/NewsFeed.js
@@ -1,13 +1,14 @@
 import React, { useEffect } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
-import PostCard from '../../custom/PostCard';
+import PostCard from "../../custom/PostCard";
+import SubNewsFeed from "./component/SubNewsFeed";
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
     width: "100%",
-    margin: 0,
+    margin: "10px 0 0 0",
   },
   paper: {
     padding: theme.spacing(2),
@@ -23,12 +24,17 @@ const NewsFeed = () => {
     <div className={classes.root}>
       <Grid container spacing={3}>
         <Grid item xs={12}>
-          <div style={{ margin: "0px -24px" }}>
-            <Grid container justify="center" spacing={3} className={classes.containRoot}>
-              <Grid item xs={4} spacing={2}>
-                {/* <SubUserInfo /> */}
+          <div style={{ margin: "0px " }}>
+            <Grid
+              container
+              justify="center"
+              spacing={5}
+              className={classes.containRoot}
+            >
+              <Grid item xs={4}>
+                <SubNewsFeed />
               </Grid>
-              <Grid item xs={7}>
+              <Grid item xs={8}>
                 {[1, 2, 3, 4, 5, 6].map((item) => (
                   <Grid item style={{ marginBottom: "20px" }}>
                     <PostCard />
